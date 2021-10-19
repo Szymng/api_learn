41f54910fddc8449725a06f482dff77ec81e5ea0
Szymng <nguyentienanh341@gmail.com>
2021-10-19 23:41:17 +0200

    Strip lines

--- spotify_api.py
+++ spotify_api.py
@@ -1,10 +1,11 @@
 """Spotify API"""
+import requests
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 with open('key.txt', 'r') as file:
-    client_ID = file.readline()
-    client_secret = file.readline()
+    client_ID = file.readline().strip()
+    client_secret = file.readline().strip()
 
 credentials_manager = SpotifyClientCredentials(client_id=client_ID, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=credentials_manager)
