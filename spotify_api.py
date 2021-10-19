cc9f708ebfd7c075189202959cf322311c7c5b05
Szymng <nguyentienanh341@gmail.com>
2021-10-19 17:38:28 +0200

    Hide credentials

--- spotify_api.py
+++ spotify_api.py
@@ -2,7 +2,9 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-client_ID = 'e385f499ac7d4707b9fe728b87e0b082'
-client_secret = '4957616893f4447d9dade1bb492c62f7'
+with open('key.txt', 'r') as file:
+    client_ID = file.readline()
+    client_secret = file.readline()
+
 credentials_manager = SpotifyClientCredentials(client_id=client_ID, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=credentials_manager)
